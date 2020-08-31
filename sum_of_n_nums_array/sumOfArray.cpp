10faeb82e8db8b166f5efbbb46cc096410434cf2
iamSK77 <sk77.mobilefix@gmail.com>
2020-08-31 16:08:52 +0530

    sum of array

---
+++ sum_of_n_nums_array/sumOfArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int n = 5;
+
+int add(int a[], int n) {
+    int s = 0;
+    for (int i = 0; i < n; i++)
+        s = s + a[i];
+        return s;
+}
+
+int main() {
+    int a[n] = {8, 2, 4, 77, 10};
+    int n = sizeof(a) / sizeof(a[0]);
+    cout << add(a, n);
+    return 0;
+}
